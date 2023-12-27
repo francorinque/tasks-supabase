0495b99eb959c07c73c37b9a37ea3bd401e48fa0
francorinque <francoagustinrinquecorreo@gmail.com>
2023-12-27 15:59:19 -0300

    add: hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 import "./App.css"
 import TasksProvider from "./context/TasksContext.jsx"
@@ -9,9 +9,9 @@ import TasksProvider from "./context/TasksContext.jsx"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TasksProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </TasksProvider>
   </React.StrictMode>
 )
